5dc2bd19daf4f04259f34d655b8fcccc11a9a121
Luis Alfaro <52721684+luisguztav20@users.noreply.github.com>
2024-10-30 17:16:18 -0600

    first commit

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,14 +2,14 @@
   <q-page class="flex flex-center">
     <img
       alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+      src="~assets/you-access-logo.svg"
+      style="width: 300px; height: 300px"
+    />
   </q-page>
 </template>
 
 <script setup>
 defineOptions({
-  name: 'IndexPage'
+  name: "IndexPage",
 });
 </script>
