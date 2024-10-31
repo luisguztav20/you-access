83b75eadf880b06791c142501e6fa8346cfbfb5b
Luis Alfaro <52721684+luisguztav20@users.noreply.github.com>
2024-10-30 19:35:23 -0600

    logo
    Importacion de logo en la carpeta assets

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,7 +2,7 @@
   <q-page class="flex flex-center">
     <img
       alt="Quasar logo"
-      src="~assets/you-access-logo.svg"
+      src="~assets/you-access-logo-01.svg"
       style="width: 300px; height: 300px"
     />
   </q-page>
