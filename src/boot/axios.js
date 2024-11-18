22be0ff45523b1bc249a6889bb59170fd0b1301e
Luis Alfaro <52721684+luisguztav20@users.noreply.github.com>
2024-11-18 11:14:55 -0600

    conexion al backend

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,7 +1,9 @@
 import { boot } from "quasar/wrappers";
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:3000" });
+const api = axios.create({
+  baseURL: "https://mw2rv8bf-3000.use2.devtunnels.ms",
+});
 
 export default boot(({ app }) => {});
 
